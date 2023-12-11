7791dde2eab5d52c1b8205bd0306e0d6b71ae8c5
Gabriel Almeida <gabrieltux19@gmail.com>
2023-12-10 22:44:15 -0300

    First iteration of GUI with Tkinter

--- replace_text_autocad.py
+++ replace_text_autocad.py
@@ -3,6 +3,9 @@ from pyautocad import Autocad
 import win32com.client
 import os
 import glob
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
 def find_and_replace(dwg_files, old_text, new_text):
     acad = win32com.client.Dispatch("AutoCAD.Application") 
@@ -42,11 +45,55 @@ def get_dwg_files(parent_folder):
 
     return dwg_files
 
-def main():
-    parent_folder = input("Digite o diretório pai: ")
-    old_text = input("Digite o texto antigo: ")
-    new_text = input("Digite o texto novo: ")
+def select_folder(entry_path):
+    selected_folder = filedialog.askdirectory()
+    entry_path.config(state='normal')
+    entry_path.delete(0, tk.END)
+    entry_path.insert(0, selected_folder)
+    entry_path.config(state='readonly')
+
+def get_parameters(entry_path, entry_old_text, entry_new_text):
+    parent_folder = entry_path.get()
+    old_text = entry_old_text.get()
+    new_text = entry_new_text.get()
+
+    start(parent_folder, old_text, new_text)
+
+def start(parent_folder, old_text, new_text):
     dwg_files = get_dwg_files(parent_folder)
     find_and_replace(dwg_files, old_text, new_text)
+    messagebox.showinfo("Processo concluído", "Todas as ocorrências encontradas foram substituídas.")
+
+def main():
+    window = tk.Tk()
+    window.title("Substituir Texto no AutoCAD")
+
+    # ENTRY AND BUTTON FOR SELECTING THE PARENT FOLDER
+    label = tk.Label(window, text="Escolha o diretório pai:")
+    label.grid(row=0, column=0, padx=10, sticky="w")
+
+    entry_path = tk.Entry(window, state='readonly', width=50)
+    entry_path.grid(row=1, column=0, padx=10, pady=10)
+
+    select_button = tk.Button(window, text="Selecionar pasta", command=lambda: select_folder(entry_path))
+    select_button.grid(row=1, column=1, padx=10, pady=10)
+
+    #ENTRYS FOR TYPING THE NEW AND OLD TEXT
+    label = tk.Label(window, text="Digite o texto a ser substituído:")
+    label.grid(row=2, column=0, padx=10, sticky="w")
+
+    entry_old_text = tk.Entry(window, state='normal', width=50)
+    entry_old_text.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+
+    label = tk.Label(window, text="Digite o novo texto:")
+    label.grid(row=4, column=0, padx=10, sticky="w")
+
+    entry_new_text = tk.Entry(window, state='normal', width=50)
+    entry_new_text.grid(row=5, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+
+    replace_button = tk.Button(window, text="Localizar e substituir", command=lambda: get_parameters(entry_path, entry_old_text, entry_new_text))
+    replace_button.grid(row=6, column=0, padx=10, pady=10, columnspan=2)
+
+    window.mainloop()
 
 main()
